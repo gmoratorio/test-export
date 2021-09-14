e8a367bec988081cc85f650be071207c6399f0e4
gmoratorio <moratorio.guillermo@gmail.com>
2021-09-14 09:13:16 -0600

    initial skeleton test

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// import * as testHelper from './helpers/testHelper';
-// import * as testService from './services/testService';
-export * from './helpers/testHelper';
-export * from './services/testService';
-// const helpers = {
-//     testHelper
-// };
-//
-// const services = {
-//     testService
-// }
-//
-// export {helpers, services};
+import * as testHelper from './helpers/testHelper';
+import * as testService from './services/testService';
+
+const helpers = {
+    testHelper
+};
+
+const services = {
+    testService
+}
+
+export {helpers, services};
